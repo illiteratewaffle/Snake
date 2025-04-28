df8e078536ce4e25aeefe4bc1c482d0261eec209
harry <153475377+illiteratewaffle@users.noreply.github.com>
2025-04-27 20:53:38 -0600

    Add menu soundtrack

--- src/main/java/controllers/MenuController.java
+++ src/main/java/controllers/MenuController.java
@@ -9,6 +9,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +20,8 @@ import java.util.ResourceBundle;
 
 public class MenuController implements Initializable {
 
+    private MediaPlayer menuMusic;
+
     @FXML
     private Button playButton;
     @FXML
@@ -25,10 +29,30 @@ public class MenuController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // generate top score
         topScoreDisplay.setText("TOP SCORE: " + TopScore.getScoreFromFile());
+
+        // play music
+        try {
+            URL resource = getClass().getResource("/menu_soundtrack.mp3");
+            if (resource != null) {
+                Media media = new Media(resource.toExternalForm());
+                menuMusic = new MediaPlayer(media);
+                menuMusic.setCycleCount(MediaPlayer.INDEFINITE);
+                menuMusic.setVolume(0.3);
+                menuMusic.play();
+            } else {
+                System.out.println("Menu music file not found!");
+            }
+        } catch (Exception e) {
+            System.out.println("Error loading menu music.");
+            e.printStackTrace();
+        }
     }
 
     public void play(ActionEvent event) throws IOException {
+        menuMusic.stop();
+
         Parent newRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("board.fxml")));
 
         Stage stage = (Stage) playButton.getScene().getWindow();
